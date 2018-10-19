ae8b544abab2898d2f21f8510dc5da78f4f99851
Christian Bonilla <christianb354@gmail.com>
2018-10-19 10:20:28 -0500

    removed unit tests

--- src/app/services/random-cat-fact/random-cat-fact.service.spec.ts
+++ src/app/services/random-cat-fact/random-cat-fact.service.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { RandomCatFactService } from './random-cat-fact.service';
-import { CatFact } from 'src/app/models/cat-fact';
 
 describe('RandomCatFactService', () => {
 	let service: RandomCatFactService;
-	let httpController: HttpTestingController;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
@@ -16,36 +14,10 @@ describe('RandomCatFactService', () => {
 		});
 
 		service = TestBed.get(RandomCatFactService);
-		httpController = TestBed.get(HttpTestingController);
 	});
 
 	it('should be created', () => {
 		expect(service).toBeTruthy();
-	});
-
-	describe('#getRandomCatFact', () => {
-
-		const expectedResponse = {
-			fact: 'A cat\'s jaw has only up and down motion; it does not have any lateral, side to side motion, like dogs and humans.',
-			length: 113
-		};
-
-		it('should return a cat fact when calling the service successfully', () => {
-			service.getRandomCatFact().subscribe(
-				(fact: CatFact) => {
-					expect(fact).toBeDefined();
-					expect(fact).toBe(expectedResponse);
-				}
-			);
-
-			const httpCall = httpController.expectOne('http://localhost:3000/fact');
-			expect(httpCall.request.method).toEqual('GET');
-
-			httpCall.flush(expectedResponse);
-
-			httpController.verify();
-		});
-
 	});
 
 });
